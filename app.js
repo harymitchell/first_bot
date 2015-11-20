69d2229de4e3d9bfabc1b0c9d5fb736f78fb67c4
Harrison Mitchell <harymitchell@gmail.com>
2015-11-20 16:52:13 -0500

    Migrated to newest express

--- app.js
+++ app.js
@@ -3,14 +3,11 @@
 */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var https= require('https');
 var path = require('path');
 var fs = require('fs');
-var routes = require('./routes');
-var privateKey  = fs.readFileSync('sslcert/key.pem', 'utf8');
+var privateKey = fs.readFileSync('sslcert/key.pem', 'utf8');
 var certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
 var options = {
     key: privateKey,
@@ -24,29 +21,21 @@ var httpsServer = https.createServer(options, app);
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 //app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 var s = fs.readFileSync("search.html", "utf-8");
 
 app.get('/', function(request, response) {
-    response.send(s);
+    response.render('index');
 });
 
 app.post('/comment', function(req, res){
 
 });
-
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
@@ -55,3 +44,37 @@ http.createServer(app).listen(app.get('port'), function(){
 httpsServer.listen(8443, function(){
     console.log('Express server listening on port 8443 https');
 });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
